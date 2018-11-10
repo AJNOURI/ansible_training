f8f38970d8619f6345ac0867d7d1890ebc4311d1
ajnouri <ajn.bin@gmail.com>
2018-11-10 08:20:44 +0100

    cleaning, general playbooks and role playbooks

--- create_ansible_user.sh
+++ create_ansible_user.sh
@@ -1,11 +1,12 @@
-> #!/bin/bash
-> 
-> AUSER=ansibleuser
-> PASSWD=ansibleuserpasswd
-> useradd $AUSER && echo $AUSER:"$PASSWD" | chpasswd
-> usermod -s /bin/bash $AUSER
-> mkdir /home/$AUSER
-> cp /etc/skel/.* /home/$AUSER
-> chown -R $AUSER:$AUSER /home/$AUSER 
-> echo "$AUSER ALL=(ALL:ALL) PASSWD: ALL" >> /etc/sudoers
-> 
+#!/bin/bash                                                                                                                                              
+#set -ex                                                                                                                                                 
+                                                                                                                                                         
+                                                                                                                                                         
+AUSER=user5 
+useradd $AUSER
+passwd $AUSER
+usermod -s /bin/bash $AUSER
+mkdir /home/$AUSER
+cp /etc/skel/.* /home/$AUSER
+chown -R $AUSER:$AUSER /home/$AUSER 
+echo "$AUSER ALL=(ALL:ALL) PASSWD: ALL" >> /etc/sudoers
